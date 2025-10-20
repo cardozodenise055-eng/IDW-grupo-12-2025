5ad653e6b6ae88d75ca92a8a9c0e95a6ffa12cc6
[Nicole <[nicolee281205@gmail.com]>
2025-10-20 20:32:36 -0300

    usuarios

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ const usuario = document.getElementById("usuario");
 const clave = document.getElementById("clave");
 const mensaje = document.getElementById("mensaje");
 
-function mostrarmensaje(texto, tipo = "danger"){
+function mostrarmensaje(texto, tipo){
     mensaje.innerHTML = `
            <div class="col-md-6 col-lg-4">
                <div class="alert alert-${tipo}">${texto}</div>
@@ -14,5 +14,17 @@ formlogin.addEventListener('submit', function(event){
     event.preventDefault();
 
     let usuarioinput = usuario.value.trim();
-    let clave = clave.value.trim();
+    let clave_ = clave.value.trim();
+
+    const isusuario = usuarios.find(
+        u => u.usuario === usuarioinput && u.clave === clave_
+    );
+
+    if(isusuario){
+        sessionStorage.setItem("usuarioLogueado", usuarioinput);
+        mostrarmensaje(`bienvenido usuario ${usuarioinput}`,"success");
+        window.location.href = "formulariomedicos.html";
+    } else{
+        mostrarmensaje('error de credenciales', "danger")
+    }
 })
